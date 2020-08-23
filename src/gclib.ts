77c70b3bc642dfeab444764566d5f2f8d3401385
jgonte <jgonte@gmail.com>
2020-08-23 10:13:59 -0400

    Added semicolons to the imports

--- src/gclib.ts
+++ src/gclib.ts
@@ -2,16 +2,16 @@
 // import "core-js/fn/array.find"
 // ...
 
-import OidcClientAuthProvider from './auth/OidcClientAuthProvider'
-import OidcClientAuthProviderConfig from './auth/OidcClientAuthProviderConfig'
-import AppUser from './auth/AppUser'
-import RolePolicy from './auth/RolePolicy'
-import RequestHeader from './data/RequestHeader'
-import CollectionLoaderConfig from './data/loaders/CollectionLoaderConfig'
-import CollectionLoader from './data/loaders/CollectionLoader'
-import SingleItemLoaderConfig from './data/loaders/SingleItemLoaderConfig'
-import SingleItemLoader from './data/loaders/SingleItemLoader'
-import { AndFilter, OrFilter } from './data/filters/LogicalFilter'
+import OidcClientAuthProvider from './auth/OidcClientAuthProvider';
+import OidcClientAuthProviderConfig from './auth/OidcClientAuthProviderConfig';
+import AppUser from './auth/AppUser';
+import RolePolicy from './auth/RolePolicy';
+import RequestHeader from './data/RequestHeader';
+import CollectionLoaderConfig from './data/loaders/CollectionLoaderConfig';
+import CollectionLoader from './data/loaders/CollectionLoader';
+import SingleItemLoaderConfig from './data/loaders/SingleItemLoaderConfig';
+import SingleItemLoader from './data/loaders/SingleItemLoader';
+import { AndFilter, OrFilter } from './data/filters/LogicalFilter';
 import {
   IsEqualFilter,
   IsNotEqualFilter,
@@ -19,27 +19,31 @@ import {
   IsGreaterOrEqualFilter,
   IsLessThanFilter,
   IsLessOrEqualFilter
-} from './data/filters/ComparisonFilter'
-import SubmitterConfig from './data/submitters/SubmitterConfig'
-import Submitter from './data/submitters/Submitter'
-import appCtrl from './app/appCtrl'
-import IntlProvider from './intl/IntlProvider'
-import IntlSubscriber from './intl/IntlSubscriber'
-import { Constructor } from './utils/Constructor'
+} from './data/filters/ComparisonFilter';
+import SubmitterConfig from './data/submitters/SubmitterConfig';
+import Submitter from './data/submitters/Submitter';
+import appCtrl from './app/appCtrl';
+import IntlProvider from './intl/IntlProvider';
+import IntlSubscriber from './intl/IntlSubscriber';
+import { Constructor } from './utils/Constructor';
 
 export {
+
   // auth
   OidcClientAuthProvider,
   OidcClientAuthProviderConfig,
   AppUser,
   RolePolicy,
+
   // data
   RequestHeader,
+
   // loaders
   CollectionLoaderConfig,
   CollectionLoader,
   SingleItemLoaderConfig,
   SingleItemLoader,
+
   // filters
   AndFilter,
   OrFilter,
@@ -49,14 +53,18 @@ export {
   IsGreaterOrEqualFilter,
   IsLessThanFilter,
   IsLessOrEqualFilter,
+
   // submitters
   SubmitterConfig,
   Submitter,
+
   // app
   appCtrl,
+
   // intl
   IntlProvider,
   IntlSubscriber,
+  
   // utils
   Constructor
 }
